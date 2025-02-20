3524ed928d16dee000e8011946420c85b7dcbd94
heskett <heskett@arc-infra-3.local>
2025-02-20 11:24:18 -0800

    scriptz

---
+++ acp_analysis/slurm.call.bwa.hg38.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --partition=batch
+#SBATCH --mem-per-cpu 6G
+#SBATCH -c 16
+#SBATCH --time=24:0:0
+#SBATCH -A thayerlab
+
+fq1=$1
+fq2=$2
+name=$3
+ref='/home/exacloud/gscratch/ThayerLab/heskett/acp_analysis/hg38_fasta_ref/hg38.fa'
+out_dir=$4
+### setting the ID, SM, and LB just to the sample name
+readgroup='@RG\tID:'$name'\tSM:'$name'\tPL:'illumina'\tLB:'$name
+
+source activate for_bwa
+bwa mem -R $readgroup -t 16 -Ma $ref $fq1 $fq2 > $out_dir$name.sam
